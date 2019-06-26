de4da20f2cab05abbc4ccb7ffacc38db9fa54518
hayate4th <dia.hayayer@gmail.com>
2019-06-27 00:48:51 +0900

    Change property of RadioButtonWithLabel component

--- src/components/RadioButtonWithLabel.tsx
+++ src/components/RadioButtonWithLabel.tsx
@@ -1,17 +1,18 @@
 import React, { Fragment } from 'react';
-import RadioButton, { RadioButtonProps } from './RadioButton';
+import RadioButton from './RadioButton';
 
 export interface RadioButtonWithLabelProps {
-  radioButtonProps: RadioButtonProps;
+  inputName: string;
+  inputValue: string;
   labelText: string;
 }
 
 const RadioButtonWithLabel: React.FC<RadioButtonWithLabelProps> = props => {
-  const { radioButtonProps, labelText } = props;
+  const { inputName, inputValue, labelText } = props;
 
   return (
     <Fragment>
-      <RadioButton inputName={radioButtonProps.inputName} inputValue={radioButtonProps.inputValue} />
+      <RadioButton inputName={inputName} inputValue={inputValue} />
       <label>{labelText}</label>
     </Fragment>
   );
