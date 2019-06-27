9c194eebeb797cda723650f889999123e59721f8
r001378 <ryotaro-nakagawa@cybozu.co.jp>
2019-06-27 11:23:23 +0900

    Add isChecked property to RadioButton component

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -3,13 +3,19 @@ import React from 'react';
 export interface RadioButtonProps {
   inputName: string;
   inputValue: string;
+  isChecked?: boolean;
 }
 
 const RadioButton: React.FC<RadioButtonProps> = props => {
-  const { inputName, inputValue } = props;
+  const { inputName, inputValue, isChecked } = props;
 
   return (
-    <input type="radio" name={inputName} value={inputValue} checked={true} />
+    <input
+      type="radio"
+      name={inputName}
+      value={inputValue}
+      checked={isChecked}
+    />
   );
 };
 
