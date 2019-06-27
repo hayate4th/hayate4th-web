5de2f3b24eae07ae6507ae5a3e93a378cbec40d3
hayate4th <dia.hayayer@gmail.com>
2019-06-27 11:34:44 +0900

    Add isChecked to RadioButton for compatibility

--- src/components/RadioButtonWithLabel.tsx
+++ src/components/RadioButtonWithLabel.tsx
@@ -1,18 +1,21 @@
 import React, { Fragment } from 'react';
-import RadioButton from './RadioButton';
+import RadioButton, { RadioButtonProps } from './RadioButton';
 
 export interface RadioButtonWithLabelProps {
-  inputName: string;
-  inputValue: string;
+  radioButtonProps: RadioButtonProps;
   labelText: string;
 }
 
 const RadioButtonWithLabel: React.FC<RadioButtonWithLabelProps> = props => {
-  const { inputName, inputValue, labelText } = props;
+  const { radioButtonProps, labelText } = props;
 
   return (
     <Fragment>
-      <RadioButton inputName={inputName} inputValue={inputValue} />
+      <RadioButton
+        inputName={radioButtonProps.inputName}
+        inputValue={radioButtonProps.inputValue}
+        isChecked={radioButtonProps.isChecked}
+      />
       <label>{labelText}</label>
     </Fragment>
   );
