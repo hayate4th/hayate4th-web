97a248d1c97d73b54d308d209d1b68dd2c1f47f4
hayate4th <dia.hayayer@gmail.com>
2019-06-27 16:06:09 +0900

    Remove unnecessary code in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import LocaleSelector from './components/LocaleSelector';
-import logo from './logo.svg';
 
 const App: React.FC = () => {
   const localeProps = [
@@ -19,20 +18,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <LocaleSelector localeProps={localeProps} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 };
