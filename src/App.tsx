a5b13eacc8b51fcce549b2a8ad9e86a121218550
hayate4th <dia.hayayer@gmail.com>
2019-06-28 16:57:39 +0900

    Add MainTitle component and animation for fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import anime from 'animejs';
 import React, { useState } from 'react';
 import './App.css';
+import MainTitle from './components/Atoms/MainTitle';
 import RoundIcon from './components/Atoms/RoundIcon';
 import Header from './components/Organisms/Header';
 import logo from './images/hayate4th.jpg';
@@ -12,6 +13,21 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <MainTitle
+        titleText="hayate4th"
+        refFunction={ref => {
+          anime({
+            delay: (el, i) => {
+              return 150 * (i + 1);
+            },
+            duration: 600,
+            easing: 'easeOutExpo',
+            offset: '-=775',
+            opacity: [0, 1],
+            targets: '.letter'
+          });
+        }}
+      />
       <RoundIcon
         urlString={logo}
         altString="hayate4th's logo"
---
+++ src/components/Atoms/MainTitle/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css';
+
+export interface MainTitleProps {
+  titleText: string;
+  refFunction?: (ref: HTMLImageElement | null) => any;
+}
+
+const MainTitle: React.FC<MainTitleProps> = props => {
+  const { titleText, refFunction } = props;
+
+  return (
+    <h1 ref={refFunction}>
+      <span className="text-wrapper">
+        <span className="letters">
+          {titleText.split('').map((value: string, key: number) => {
+            return (
+              <span key={key} className="letter">
+                {value}
+              </span>
+            );
+          })}
+        </span>
+      </span>
+    </h1>
+  );
+};
+
+export default MainTitle;
