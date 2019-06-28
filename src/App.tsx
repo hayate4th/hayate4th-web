ca30a9eca5d60bd9bf9d508bf0f3fcced4c453e6
hayate4th <dia.hayayer@gmail.com>
2019-06-28 09:58:35 +0900

    Change padding to be set on outer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import RoundIcon from './components/RoundIcon';
 import logo from './images/hayate4th.jpg';
