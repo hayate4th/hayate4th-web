772f906d16b523fa83a74847ad49847a55a02303
hayate4th <dia.hayayer@gmail.com>
2019-06-28 13:39:32 +0900

    Add animation to RoundIcon component in main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import anime from 'animejs';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import RoundIcon from './components/RoundIcon';
 import logo from './images/hayate4th.jpg';
 
 const App: React.FC = () => {
+  const [windowWidth] = useState(window.innerWidth);
+
   return (
     <div className="App">
       <Header />
@@ -13,6 +16,13 @@ const App: React.FC = () => {
         altString="hayate4th's logo"
         width={300}
         height={300}
+        refFunction={ref => {
+          anime({
+            duration: 1500,
+            targets: ref,
+            translateX: [windowWidth, 0]
+          });
+        }}
       />
     </div>
   );
