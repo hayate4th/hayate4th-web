72519886f43a9dcd08bb2f06705608746b3c2023
hayate4th <dia.hayayer@gmail.com>
2019-07-01 00:13:36 +0900

    Change LocaleSelector to use redux store

--- src/components/Molecules/LocaleSelector/index.tsx
+++ src/components/Molecules/LocaleSelector/index.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { Action } from 'typescript-fsa';
+import { localeActions } from '../../../actions/localeActions';
+import { LocaleState } from '../../../states/localeStates';
+import { AppState } from '../../../store';
 import { IndividualRadioButtonProps } from '../../Atoms/RadioButon';
 import RadioButtonGroup from '../RadioButtonGroup';
 
-export interface LocaleSelectorProps {
+export interface LocaleActions {
+  updateLocale: (v: string) => Action<string>;
+}
+
+export interface LocaleSelectorProps extends LocaleState, LocaleActions {
   localeProps: IndividualRadioButtonProps[];
 }
 
 const LocaleSelector: React.FC<LocaleSelectorProps> = props => {
-  const { localeProps } = props;
+  const { localeProps, locale, updateLocale } = props;
+  const { i18n } = useTranslation();
 
   return (
     <div
-      className="locale-selector"
-      onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-        console.log(event.target.value)
-      }
+      // TODO: 別の書き方を探したい
+      className={'locale-selector ' + locale}
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+        updateLocale(event.target.value);
+        // TODO: 確実に updateLocale が完了してから changeLanguage するようにする
+        i18n.changeLanguage(event.target.value);
+      }}
     >
       <RadioButtonGroup
         inputGroupName="locale"
@@ -24,4 +39,17 @@ const LocaleSelector: React.FC<LocaleSelectorProps> = props => {
   );
 };
 
-export default LocaleSelector;
+const mapDispatchToProps = (dispatch: Dispatch<Action<string>>) => {
+  return {
+    updateLocale: (v: string) => dispatch(localeActions.updateLocale(v))
+  };
+};
+
+const mapStateToProps = (appState: AppState) => {
+  return Object.assign({}, appState.locale);
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LocaleSelector);
