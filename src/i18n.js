287d41a0e1410251cba670c3c3db32b09146faa5
hayate4th <dia.hayayer@gmail.com>
2019-07-05 22:22:32 +0900

    Add new text in i18n file

--- src/i18n.js
+++ src/i18n.js
@@ -12,12 +12,30 @@ i18n.use(initReactI18next).init({
     resources: {
         en: {
             translations: {
-                "Main Title": "hayate4th's room"
+                "Main Title": "hayate4th's room",
+                "Education Title": "Education",
+                "Education Junior High": "Hiroo Gakuen Junior High School",
+                "Education Junior High Period": "2008 Apr - 2011 Mar",
+                "Education High": "Hiroo Gakuen Senior High School",
+                "Education High Period": "2011 Apr - 2014 Mar",
+                "Education University": "College of Information Science, University of Tsukuba",
+                "Education University Period": "2014 Apr - 2018 Mar",
+                "Education Graduate": "Department of Computer Science, Graduate school of Systems and Information Science, University of Tsukuba (leave)",
+                "Education Graduate Period": "2018 Apr - 2019 Mar"
             }
         },
         ja: {
             translations: {
-                "Main Title": "hayate4thの部屋"
+                "Main Title": "hayate4thの部屋",
+                "Education Title": "学歴",
+                "Education Junior High": "広尾学園 中学校",
+                "Education Junior High Period": "2008.04 - 2011.03",
+                "Education High": "広尾学園 高等学校",
+                "Education High Period": "2011.04 - 2014.03",
+                "Education University": "筑波大学 情報学群 情報科学類",
+                "Education University Period": "2014.04 - 2018.03",
+                "Education Graduate": "筑波大学大学院 システム情報工学研究科 コンピュータサイエンス専攻 (中途退学)",
+                "Education Graduate Period": "2018.04 - 2019.03"
             }
         }
     }
